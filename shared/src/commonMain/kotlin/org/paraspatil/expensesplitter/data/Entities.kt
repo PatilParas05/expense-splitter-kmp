c766cf86b7b22c79f0a3e3a150a5e938ed641a04
PatilParas05 <parasspatil.111@gmail.com>
2026-05-01 17:21:16 +0530

    feat: Create file Entities for define database entities.

---
+++ shared/src/commonMain/kotlin/org/paraspatil/expensesplitter/data/Entities.kt
@@ -0,0 +1,18 @@
+package org.paraspatil.expensesplitter.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class PersonEntity(
+    @PrimaryKey val id: String,
+    val name: String
+)
+
+@Entity
+data class ExpenseEntity(
+    @PrimaryKey val id: String,
+    val amount: Double,
+    val paidBy: String,
+    val description: String
+)
